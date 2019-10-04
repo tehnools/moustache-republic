439269d11b41aa1d0d1afea0609574246773e379
noel <noelsoong@live.com>
2019-10-04 16:05:36 +1300

    Temp data change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar'
 import ProductPage from './components/ProductPage'
 
 const item = {
+  id: 1,
   name: 'Classic Tee',
   description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempore id, molestias reiciendis enim
   voluptatem est eaque sunt, perferendis voluptate quis recusandae explicabo,
