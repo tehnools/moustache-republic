7e016c6b54db0723d3586fba54bc2c11db1f2b59
noel <noelsoong@live.com>
2019-10-04 15:07:34 +1300

    temporary props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css'
 import NavBar from './components/NavBar'
 import ProductPage from './components/ProductPage'
 
+const item = {
+  name: 'Classic Tee',
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempore id, molestias reiciendis enim
+  voluptatem est eaque sunt, perferendis voluptate quis recusandae explicabo,
+  quae saepe sed eum ullam! In, dicta!`,
+  price: 10
+}
+
 function App () {
   return (
     <div className="App">
       <NavBar />
-      <ProductPage/>
+      <ProductPage item={item}/>
     </div>
   )
 }
